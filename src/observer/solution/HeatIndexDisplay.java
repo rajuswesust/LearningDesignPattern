c13ed2fcc808deb5059afd15a5d482f1cb326cc3
rajuswesust <singhoraju@gmail.com>
2025-01-22 01:01:49 +0600

    observer- add new display to understand flexibility

---
+++ src/observer/solution/HeatIndexDisplay.java
@@ -0,0 +1,42 @@
+package observer.solution;
+
+public class HeatIndexDisplay implements Observer, Display{
+
+    double heatIndex;
+    private WeatherData weatherData;
+
+    public HeatIndexDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.addObserver(this);
+    }
+
+    @Override
+    public void update(double temperature, double humidity, double pressure) {
+        heatIndex = computeHeatIndex(temperature, humidity);
+        display();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("--------------------------------");
+        System.out.println("Feels like: " + heatIndex + "F degrees");
+        System.out.println("--------------------------------");
+    }
+
+    private double computeHeatIndex(double t, double rh) {
+        //converting to Fahrenheit
+        t = t*(9.0 / 5.0) + 32;
+
+        heatIndex = (double) ((16.923 + (0.185212 * t) + (5.37941 * rh) - (0.100254 * t * rh)
+                + (0.00941695 * (t * t)) + (0.00728898 * (rh * rh))
+                + (0.000345372 * (t * t * rh)) - (0.000814971 * (t * rh * rh)) +
+                (0.0000102102 * (t * t * rh * rh)) - (0.000038646 * (t * t * t)) + (0.0000291583 *
+                (rh * rh * rh)) + (0.00000142721 * (t * t * t * rh)) +
+                (0.000000197483 * (t * rh * rh * rh)) - (0.0000000218429 * (t * t * t * rh * rh)) +
+                0.000000000843296 * (t * t * rh * rh * rh)) -
+                (0.0000000000481975 * (t * t * t * rh * rh * rh)));
+
+        //converting to Celsius, because nobody reads in Fahrenheit
+        return ((heatIndex - 32)/1.8);
+    }
+}
--- src/observer/solution/ObserverPatternExample.java
+++ src/observer/solution/ObserverPatternExample.java
@@ -7,8 +7,11 @@ public class ObserverPatternExample {
         StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
         ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
 
+        //adding new display
+        HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
+
         //simulate new weather measurements
-        weatherData.setMeasurements(20.0, 75, 1012);
-        weatherData.setMeasurements(20.5, 74.5, 1012);
+        weatherData.setMeasurements(20.0, 86, 1012);
+        weatherData.setMeasurements(20.5, 86.5, 1012);
     }
 }
